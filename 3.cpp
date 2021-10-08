4ce747f4aa52a53d6bed230d624ba290836d90d4
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2021-10-08 17:10:43 +0300

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int length;
+    int width;
+    int area;
+
+    length = 7;
+    width = 5;
+
+    area = length * width;
+
+    cout << "the area of the rectangle is equal to ";
+    cout << area;
+
+    return  0;
+}
