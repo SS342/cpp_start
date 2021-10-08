d5d3d766b32365fa932c8e016aadd718c7b8417e
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2021-10-08 17:20:01 +0300

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int length;
+    int width;
+
+    cout << "input length of rectangle: ";
+    cin >> length;
+
+    cout << "input width of rectangle:";
+    cin >> width;
+
+    cout << "the area of the rectangle is equal to - ";
+    cout << length * width;
+
+    return 0;
+}
