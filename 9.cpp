adf40f8b1659f2e59000f68ce6499c69d2e2a668
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2021-10-08 18:02:42 +0300

    Create 9.cpp

---
+++ 9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int count;
+
+    for(count=1; count <= 100; count = count + 1)
+
+        cout << count << "\n";
+    
+    string a;
+    
+    cin >> a;
+    
+    return 0;
+}
