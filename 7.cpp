e2df868f8b5d6a8249de8b81269611daee028957
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2021-10-08 17:45:52 +0300

    Create 7.cpp

---
+++ 7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double f;
+    double m;
+
+    cout << "Enter the length in feet: ";
+    cin >> f;
+
+    m = f / 3.28;
+
+    cout << f << " feet equals " << m << " meters";
+
+    return 0;
+}
